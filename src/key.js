25538fa216181dbdc89ccb09e301655f4001c494
kgicheha <55036976+kgicheha@users.noreply.github.com>
2022-06-26 19:55:19 -0700

    Delete key.js

--- src/key.js
+++
@@ -1,7 +0,0 @@
-// Your dedicated access key is: 8B5WZ1MU1IOSD0DG
-
-
-urlAPIKey= `apikey=8B5WZ1MU1IOSD0DG`
-const apiURL = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=aapl&${urlAPIKey}`;
-
-// const apiURL = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=aapl&apikey=8B5WZ1MU1IOSD0DG`
